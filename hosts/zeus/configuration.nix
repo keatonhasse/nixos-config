b4ea7e758156549799361341bf845fbb5158da1e
Keaton Hasse <git@keatonhasse.com>
2025-08-12 19:49:12 -0500

    forgot semicolon

--- hosts/zeus/configuration.nix
+++ hosts/zeus/configuration.nix
@@ -19,15 +19,15 @@
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
     };
   };
 
@@ -72,9 +72,9 @@
     fish = {
       enable = true;
       interactiveShellInit = ''
-          set fish_greeting
-        '';
-    }
+        set fish_greeting
+      '';
+    };
   };
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
